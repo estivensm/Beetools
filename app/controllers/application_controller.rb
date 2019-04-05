a2a02363088710dde3d367d9214a26e8a0c6f806
alejovelez10 <alejovelez10@gmail.com>
2019-04-05 11:28:08 -0500

    layout_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+
+  def configure_permitted_parameters
+    [:account_update,:sign_up].each do |metodo|
+    devise_parameter_sanitizer.permit(metodo, keys: [:first_name, :second_name, :first_last_name, :second_last_name, :document_type, :document_number, :birthday, :avatar, :admin_user, :is_account])
+   end
+  end
+
+  layout :layout_for_selection
+	protected
+	
+	def layout_for_selection
+		if controller_name == 'sessions'  || controller_name == 'passwords' 
+		      'application'
+		elsif controller_name == 'registrations'
+
+		    if action_name == "new" || action_name == "create"
+		            'application'
+		    else
+		     'user'
+		    end
+		 else
+		  'user'
+		end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  first_name             :string
+#  second_name            :string
+#  first_last_name        :string
+#  second_last_name       :string
+#  document_type          :string
+#  document_number        :string
+#  birthday               :date
+#  position_id            :integer
+#  avatar                 :string
+#  admin_user             :integer
+#  is_account             :boolean
+#  rol_id                 :integer
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
