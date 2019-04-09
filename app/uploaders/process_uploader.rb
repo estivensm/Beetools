13aa7eccdbdc2e064936902ca29059dec6efdb50
alejovelez10 <alejovelez10@gmail.com>
2019-04-09 13:52:27 -0500

    pruebas

--- app/uploaders/process_uploader.rb
+++ app/uploaders/process_uploader.rb
@@ -5,6 +5,7 @@ class ProcessUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+  storage (Rails.env.production? ? :fog : :file)
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
