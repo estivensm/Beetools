ffa9c2daac6cc4860195fea9d60257c57517d29a
alejovelez10 <alejovelez10@gmail.com>
2019-04-10 15:52:14 -0500

    user-edit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  #respond_to :html, :js, :only => [:new, :create, :create_user]
+  respond_to :html, :js, :only => [:new, :create, :create_user, :update]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
